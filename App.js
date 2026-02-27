03945052ab3fed16d95513428440cfacd0ca7fc9
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2026-02-27 16:26:47 -0300

    Feat: Adiciona atividade contador-de-cliques

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [contador, setContador] = useState(0);
+
+  const incrementar = () => { setContador(contador + 1); }
+  const decrementar = () => { setContador(contador - 1); }
+  const reset = () => { setContador(0); }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+<View style={styles.container}>
+    <Text style={styles.titulo}>Contador Digital</Text>
+    <Text style={styles.numero}>{contador} </Text>
+
+    <View style={styles.linhaDeBotoes}>
+        <TouchableOpacity style={styles.botaoMais} onPress={decrementar}>
+            <Text style={styles.textoBotao}>-</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.botaoMenos} onPress={incrementar}>
+             <Text style={styles.textoBotao}>+</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.botaoReset} onPress={reset}>
+             <Text style={styles.textoBotao}>reset</Text>
+        </TouchableOpacity>
     </View>
+</View>
   );
 }
 
@@ -17,4 +37,36 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontSize: 30,
+    color: '#240177',
+    fontWeight: 'bold',
+  },
+  numero: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    color: '#240177',
+  },
+  linhaDeBotoes: {
+   flexDirection: 'row',
+   marginTop: 20,
+  },
+  botaoMais: {
+    backgroundColor: 'rgb(195, 158, 255)',
+    padding: 15,
+    marginRight: 10,
+    borderRadius: 5,
+  },
+  botaoMenos: {
+    backgroundColor: '#975bdb',
+    padding: 15,
+    marginRight: 10,
+    borderRadius: 5,
+  },
+  botaoReset: {
+    backgroundColor: '#c7c7ca',
+    padding: 15,
+    marginRight: 10,
+    borderRadius: 5,
+  }
 });
